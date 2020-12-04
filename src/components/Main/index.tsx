e4e42360bd842fa490678fabfa729ccfda275b5c
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-12-05 00:24:42 +0300

    drop styles and markup in Marks

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,67 +1,59 @@
-import {h, Fragment} from 'preact'
+import {h} from 'preact'
 import {useState} from 'preact/hooks'
+import {Marks, InfoMarks, CardinalInfo} from '../pages'
+import * as Img from '../common/images'
 import * as Type from '../../types'
 import '../../animation/index.scss'
 import './index.scss'
 
-const marks = [
-  [{id: 1, markColor: 'red', markAnimate: 'lateral', text: 'lateral red'}, {id: 2, markColor: 'green', markAnimate: 'lateral', text: 'lateral green'}],
+const marks: Type.Mark[][] = [
+  [{id: 1, markColor: 'red', markAnimate: 'lateral', text: 'lateral red', img: Img.lateralRed}, {id: 2, markColor: 'green', markAnimate: 'lateral', text: 'lateral green', img: Img.lateralGreen}],
   [
-    {id: 3, markAnimate: 'northCardinal', text: 'north cardinal'}, {id: 4, markAnimate: 'eastCardinal', text: 'east cardinal'},
-    {id: 5, markAnimate: 'southCardinal', text: 'south cardinal'}, {id: 6, markAnimate: 'westCardinal', text: 'west cardinal // пока не работает //'}
+    {id: 3, markAnimate: 'northCardinal', text: 'north cardinal', img: Img.northCardinal}, {id: 4, markAnimate: 'eastCardinal', text: 'east cardinal', img: Img.eastCardinal},
+    {id: 5, markAnimate: 'southCardinal', text: 'south cardinal', img: Img.southCardinal}, {id: 6, markAnimate: 'westCardinal', text: 'west cardinal', img: Img.westCardinal}
   ],
-  [{id: 7, markAnimate: 'isolatedDanger', text: 'isolated danger'}],
-  [{id: 8, markAnimate: 'emergencyWreckMarking', text: 'emergency wreck marking'}],
-  [{id: 9, markAnimate: 'saveWater', text: 'save water'}],
-  [{id: 10, markAnimate: 'special', text: 'special'}],
+  [{id: 7, markAnimate: 'isolatedDanger', text: 'isolated danger', img: Img.isolatedDanger}],
+  [{id: 8, markAnimate: 'emergencyWreckMarking', text: 'emergency wreck marking', img: Img.emergencyWreckMarking}],
+  [{id: 9, markAnimate: 'saveWater', text: 'save water', img: Img.saveWater}],
+  [{id: 10, markAnimate: 'special', text: 'special', img: Img.special}]
 ]
 
+enum Pages {InfoMarks, Marks, CardinalInfo}
+
 export const NavigationMarks = () => {
-  const [currentMark, setCurrentMark] = useState<Mark>({
+  const [currentMark, setCurrentMark] = useState<Type.Mark>({
     id: 1,
     markColor: 'red',
     markAnimate: 'lateral',
-    text: 'lateral red'
+    text: 'lateral red',
+    img: Img.lateralRed
   })
+  let [currentPage, setCurrentPage] = useState(Pages.InfoMarks)
+  const prev = () => {
+    setCurrentPage(--currentPage)
+  }
+  const next = () => {
+    setCurrentPage(++currentPage)
+  }
+  let i = 1 + currentPage
   return (
     <Centered>
-      <div class='window'>
-        <Circle color={currentMark.markColor} animate={currentMark.markAnimate}/>
+      {currentPage === Pages.InfoMarks && <InfoMarks />}
+      {currentPage === Pages.CardinalInfo && <CardinalInfo />}
+      {currentPage === Pages.Marks && <Marks marks={marks} ship={Img.ship} currentMark={currentMark} onCurrentMark={setCurrentMark}/>}
+      <div class='arrows has-text-centered'>
+        {currentPage > 0 && <div class='prev' onClick={prev}>туда</div>}
+        <div class='current-page'>{`${i} из 4`}</div>
+        {currentPage <= 4 && <div class='next' onClick={next}>сюда</div>}
       </div>
-      <span class='marks'>
-        {marks.map(mark => <Mark mark={mark} onCurrentMark={setCurrentMark}/>)}
-      </span>
     </Centered>
   )
 }
 
 const Centered: Type.F<{}> = ({children}) =>
   <section class='hero centered'>
-    <div class='hero-body has-text-centered'>
+    <div class='hero-body'>
       {children}
     </div>
   </section>
 
-
-type Mark = {
-  id: number
-  markColor?: string
-  markAnimate: string
-  text: string
-}
-
-type MarkProps = {
-  mark: Mark[]
-  onCurrentMark: (mark: Mark) => void
-}
-
-const Mark: Type.F<MarkProps> = ({mark, onCurrentMark}) => {
-  return (
-    <Fragment>
-      {mark.map(i => <p class='mark' onClick={() => onCurrentMark(i)}>{i.text}</p>)}
-    </Fragment>
-    )
-}
-
-const Circle: Type.F<{color: string, animate: string}> = ({color, animate}) =>
-  <div class={`circle ${color} ${animate}`}/>
