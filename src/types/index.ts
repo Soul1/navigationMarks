92bfcac04a1e5c46420553a20214886902203b29
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-12-05 00:26:48 +0300

    add new types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,18 @@
 import {FunctionalComponent} from 'preact'
 
 export type F<T> = FunctionalComponent<T>
+
+export type Mark = {
+  id: number
+  markColor?: string
+  markAnimate: string
+  text: string
+  img: string
+}
+
+export type Info = {
+  id: number
+  title: string
+  text: string
+  img: string
+}
