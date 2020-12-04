dca74caa9b379c6a4c20d68fa244f16ec76a7378
Candela (Midaev Akhmad) <darkwaud@gmail.com>
2020-12-04 15:09:18 +0300

    add images marks and convenient export

---
+++ src/components/common/images/index.ts
@@ -0,0 +1,13 @@
+import lateralRed from './lateralRed.png'
+import lateralGreen from './lateralGreen.png'
+import northCardinal from './northCardinal.png'
+import eastCardinal from './eastCardinal.png'
+import southCardinal from './southCardinal.png'
+import westCardinal from './westCardinal.png'
+import isolatedDanger from './isolatedDanger.png'
+import emergencyWreckMarking from './emergencyWreckMarking.png'
+import saveWater from './saveWater.png'
+import special from './special.png'
+import ship from './ship.png'
+
+export {lateralRed, lateralGreen, northCardinal, eastCardinal, southCardinal, westCardinal, isolatedDanger, emergencyWreckMarking, saveWater, special, ship}
